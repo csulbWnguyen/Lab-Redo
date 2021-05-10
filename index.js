02d542aa877669d127fffcd012284e9f63aecd92
csulbWnguyen <77957790+csulbWnguyen@users.noreply.github.com>
2021-05-10 00:20:06 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 const express = require("express");
 const app = express();
 
+const multer = require("multer");
+const upload = multer();
+
 // Setup view engine to ejs
 app.set('view engine', 'ejs');
 
 // Serve static content directly
 app.use(express.static("css"));
 
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
 // Setup routes
 
 // Route to welcome page
 app.get('/', (request, response) => {
     response.render("index");
 });
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 
 // Start listening to incoming requests
 // If process.env.PORT is not defined, port number 3000 is used
@@ -40,9 +56,62 @@ app.get('/services', (request, response) => {
                 name: "Security",
                 desc: "Secure your network"
             }
-        ]  
+        ]
     };
-    response.render("services", {
-        name: name,
-        data: data});
+    response.render("services",
+        {
+            name: name,
+            data: data
+        });
+}); 
+
+// GET Route to form page
+app.get('/formPost', (request, response) => {
+    const message = "get";
+    const data = {
+        name: "",
+        email: "",
+        payment: ""
+    };
+    response.render("formPost", 
+        {
+            message: message,
+            data: data
+        });
+
+});
+
+// POST Route to form page
+//app.post('/formPost', (request, response) => {
+    app.post('/formPost', upload.array(), (request, response) => {    
+        const message = "post";
+        // Send form data back to the form
+        const data = {
+            name: request.body.name,
+            email: request.body.email,
+            payment: request.body.payment
+        }
+        //Call formPost passing message and name
+        response.render("formPost", 
+            {
+                message: message,
+                data: data
+            });
+    });
+
+// GET Route to form page
+app.get('/formAjax', (request, response) => {
+    response.render("formAjax")
+});
+
+// POST Route to form page
+app.post('/formAjax', upload.array(), (request, response) => {    
+    // Send form data back to the form
+    const data = {
+        name: request.body.name,
+        email: request.body.email,
+        payment: request.body.payment
+    };
+    //Call formPost passing message and name
+    response.json(data);
 });
